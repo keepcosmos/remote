ebbb114a0d7bdf6d9d9de9eb24d9e485405e3d56
Ubuntu <ubuntu@ip-172-31-20-147.ap-northeast-2.compute.internal>
2017-11-12 06:58:48 +0000

    run mosh

--- prepare.sh
+++ prepare.sh
@@ -4,4 +4,10 @@ sudo apt-get -y update
 sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
 sudo apt-get -y install mosh
 
+echo "Set user($(whoami)) password"
 sudo passwd $(whoami)
+
+echo "Run mosh"
+mosh-server new -s
+echo "Exist and connect with mosh for mobile cli!!"
+
